08dd0cd277f4066662646ae2353780979be167c1
Bermin Agovic <bagovic@1-A-1.42heilbronn.de>
2021-07-14 11:22:32 +0200

    ft_memcpy fixed

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,24 +6,27 @@
 /*   By: bagovic <bagovic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 11:24:00 by bagovic           #+#    #+#             */
-/*   Updated: 2021/07/08 16:00:36 by bagovic          ###   ########.fr       */
+/*   Updated: 2021/07/14 11:22:16 by bagovic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-void	ft_memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
-	char		*str_dest_pointer;
-	const char	*str_src_pointer;
-	size_t		counter;
+	char		*destpointer;
+	const char	*srcpointer;
+	size_t		c;
 
-	str_dest_pointer = dest;
-	str_src_pointer = src;
-	counter = 0;
-	while (counter < n)
+	destpointer = dest;
+	srcpointer = src;
+	if (srcpointer == NULL && destpointer == NULL)
+		return (NULL);
+	c = 0;
+	while (c < n)
 	{
-		str_dest_pointer[counter] = str_src_pointer[counter];
-		counter++;
+		destpointer[c] = srcpointer[c];
+		c++;
 	}
+	return (destpointer);
 }
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: bagovic <bagovic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/06 14:54:33 by bagovic           #+#    #+#             */
-/*   Updated: 2021/07/14 11:03:25 by bagovic          ###   ########.fr       */
+/*   Updated: 2021/07/14 11:15:07 by bagovic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 
 void		*ft_memset(void *b, int c, size_t len);
 void		ft_bzero(void *s, size_t n);
-void		ft_memcpy(void *dest, const void *src, size_t n);
+void		*ft_memcpy(void *dest, const void *src, size_t n);
 void		*ft_memmove(void *dest, const void *src, size_t n);
 int			ft_isalpha(int c);
 int			ft_isdigit(int c);
