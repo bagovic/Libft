e742c22596b8e9cd49ed3bcc6bc190276b2b02c1
Bermin Agovic <bagovic@1-A-16.42heilbronn.de>
2021-07-23 18:28:49 +0200

    ft_strmapi is done.

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -6,10 +6,27 @@
 /*   By: bagovic <bagovic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/23 18:17:59 by bagovic           #+#    #+#             */
-/*   Updated: 2021/07/23 18:18:19 by bagovic          ###   ########.fr       */
+/*   Updated: 2021/07/23 18:28:09 by bagovic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdlib.h>
+#include "libft.h"
+
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
 {
+	char			*strmapi;
+	unsigned int	count;
+
+	strmapi = malloc(ft_strlen(s) + 1);
+	if (strmapi == NULL || s == NULL)
+		return (NULL);
+	count = 0;
+	while (s[count] != '\0')
+	{
+		strmapi[count] = (*f)(count, s[count]);
+		count++;
+	}
+	strmapi[count] = '\0';
+	return (strmapi);
 }
