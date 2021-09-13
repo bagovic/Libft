9f9f3587a50b0647ad1a01462a5659b33cda72da
Bermin Agovic <bagovic@2-F-5.42heilbronn.de>
2021-09-13 18:07:40 +0200

    working on ft_lstmap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,7 +6,7 @@
 /*   By: bagovic <bagovic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/05 16:46:29 by bagovic           #+#    #+#             */
-/*   Updated: 2021/09/13 12:34:01 by bagovic          ###   ########.fr       */
+/*   Updated: 2021/09/13 17:41:52 by bagovic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,36 @@
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new_list;
-	t_list	*next;
+	t_list	*newlst;
 
-	while (lst != NULL)
+	newlst = ft_lstnew(NULL);
+	while (lst)
 	{
-		new_list = malloc(sizeof (t_list));
-		if (new_list == NULL)
-			return (NULL);
-		new_list->next = NULL;
-		new_list = f(lst->content);
-		next = lst->next;
+		newlst = f(lst->content);
+		newlst = newlst->next;
 		del(lst->content);
-		free(lst);
-		lst = next;
-		new_list = new_list->next;
+		lst = lst->next;
 	}
-	return (new_list);
+	return (newlst);
 }
+
+// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+// {
+// 	t_list	*new_list;
+// 	t_list	*next;
+
+// 	while (lst != NULL)
+// 	{
+// 		new_list = malloc(sizeof (t_list));
+// 		if (new_list == NULL)
+// 			return (NULL);
+// 		new_list->next = NULL;
+// 		new_list = f(lst->content);
+// 		next = lst->next;
+// 		del(lst->content);
+// 		free(lst);
+// 		lst = next;
+// 		new_list = new_list->next;
+// 	}
+// 	return (new_list);
+// }
