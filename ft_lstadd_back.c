48ace2fd30c37b35244a7e61229fa6704d4d7b76
Bermin Agovic <bagovic@2-G-4.42heilbronn.de>
2021-09-21 18:06:29 +0200

    fixed lstadd_back

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -6,7 +6,7 @@
 /*   By: bagovic <bagovic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/04 17:07:38 by bagovic           #+#    #+#             */
-/*   Updated: 2021/08/05 15:45:48 by bagovic          ###   ########.fr       */
+/*   Updated: 2021/09/21 18:02:56 by bagovic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,15 @@
 
 void	ft_lstadd_back(t_list **lst, t_list *new)
 {
-	t_list	*current;
+	t_list	*node;
 
-	current = *lst;
-	if (!current)
+	if (*lst == NULL)
+		*lst = new;
+	else
 	{
-		current = new;
-		new = NULL;
+		node = *lst;
+		while (node->next)
+			node = node->next;
+		node->next = new;
 	}
-	while (current->next)
-		current = current->next;
-	current->next = new;
-	*lst = current;
 }
